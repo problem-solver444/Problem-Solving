2a87d2ba5b462d51a0b3f9f5d44f819892b10b61
Ahmed Ashraf <168082133+problem-solver444@users.noreply.github.com>
2025-05-01 21:37:05 +0300

    anthor problem has been solved :)

---
+++ CodeForces/icpc assuit sheet #junior1/(Bitmask, Bitset, Bits)/icpc assuit sheet #junior1 Practice/B. New Year and Old Property/Problem Solving.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#define endl "\n"
+#include <cmath>
+#include<set>
+#include<bitset>
+#include<algorithm>
+#define all(v) v.begin(), v.end()
+#define ll long long int
+using namespace std;
+const int sz = 9000000;
+const int OO = 0x3f3f3f3f;
+bool composite[sz + 1];
+void fast() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+// binary representation will be known in the program inside n  with & (00010000)left shift  while return 1 or 0 the number of bit 
+bool knowBit(ll n, ll x) {
+    return n & (1 << x);
+}
+
+ll setBitto1(ll n, ll x)//set bit to 1
+{
+    return n | (1 << x);
+}
+
+ll resetBitto0(ll n, ll x)//reset bit to 0
+{
+    return n & (~(1 << x));
+}
+
+ll resetBit2(ll n, ll x)//-1 has all of his bits 11111111   
+{
+    return n & (-1 ^ (1 << x));
+}
+
+ll flipBit(ll n, ll x) {
+    return n ^ (1 << x);
+}
+
+bool ispoweroftwo(int n) {
+    bitset<32> bits(n);
+    return (bits.count() == 1);
+}
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+
+    int count = 0;
+    for (int bits = 2; bits <= 60; bits++)
+    {
+        ll full_ones = (1LL << bits) - 1; 
+        for (int zeroPos = 0; zeroPos < bits - 1; zeroPos++) {
+            ll num = full_ones ^ (1LL << zeroPos); 
+            if (num >= a && num <= b)
+                count++;
+        }
+    }
+
+    cout << count << endl;
+}
+int main() {
+    fast();
+    solve();
+}
